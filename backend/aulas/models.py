30b157d1364ef4d3d47444d6ddc08c7305fa8bb9
btocarmona2021 <btocarmona@gmail.com>
2026-05-31 22:29:17 -0300

    Creación de modelos ReservaAula y HorarioMateria

--- backend/aulas/models.py
+++ backend/aulas/models.py
@@ -20,7 +20,21 @@ class Materia(models.Model):
 
 class Aula(models.Model):
     descripcion = models.CharField(max_length=128)
-    ubicacion = models.CharField(max_length=128, verbose_name="ubicación")
+    ubicacion = models.CharField(max_length=128)
     cant_proyector = models.IntegerField(default=0)
     aforo = models.IntegerField(default=0)
     es_climatizada = models.BooleanField(default=False)
+
+
+class ReservaAula(models.Model):
+    id_aula = models.ForeignKey(Aula, on_delete=models.CASCADE)
+    fh_desde = models.DateTimeField()
+    fh_hasta = models.DateTimeField()
+    observacion = models.CharField(max_length=256)
+
+
+class HorarioMateria(models.Model):
+    id_materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    id_reserva = models.ForeignKey(ReservaAula, on_delete=models.CASCADE)
+    fh_desde = models.DateTimeField()
+    fh_hasta = models.DateTimeField()
